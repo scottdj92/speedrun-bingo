13eeaefc48c17119ea5dc2a53bc90b9048f8231f
Scott Jones <scottdj92@gmail.com>
2017-03-21 00:11:39 -0400

    Modify table to be aware of addMilestone event. Do we need input-field after this?

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import App from './app/app';
 // import styles
 import './app/_main.scss';
 
-App.renderSync().appendTo(document.body);
+App.renderSync({
+  $global: {
+    data: [
+      null
+    ]
+  }
+}).appendTo(document.body);
