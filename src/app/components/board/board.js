a7382af1d6428e9ed5017ba9acb6728ed5b19258
Michael Rawlings <mrawlingst@gmail.com>
2017-04-01 23:48:03 -0400

    Change input to accept numbers only

--- src/app/components/board/board.js
+++ src/app/components/board/board.js
@@ -68,7 +68,7 @@ export default class Board extends Component {
                   <div className="field">
                       <label className="label">Seed</label>
                       <p className="control">
-                          <input className="input" type="text" name="seed" placeholder="Leave blank for a random seed" onChange={this.handleChange.bind(this)}/>
+                          <input className="input" type="number" name="seed" placeholder="Leave blank for a random seed" onChange={this.handleChange.bind(this)}/>
                       </p>
                   </div>
                   <div className="field">
